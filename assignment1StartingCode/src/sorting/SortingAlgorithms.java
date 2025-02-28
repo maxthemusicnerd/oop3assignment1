47a2df8892d612836426f35160be686d66edeef2
Dakari Sow <dddddakari@gmail.com>
2025-02-27 18:14:18 -0700

    insertion sort
    the completed insertion sort

--- assignment1StartingCode/src/sorting/SortingAlgorithms.java
+++ assignment1StartingCode/src/sorting/SortingAlgorithms.java
@@ -3,5 +3,20 @@ package sorting;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class SortingAlgorithms {}
+public class SortingAlgorithms {
+	
+	
+    // Insertion Sort
+    public static <T> void insertionSort(T[] array, Comparator<T> comparator) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            T key = array[i];
+            int j = i - 1;
+            while (j >= 0 && comparator.compare(array[j], key) < 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
