0c6ce0ad501d24c16ff87611d3c71618acfeb617
Dakari Sow <dddddakari@gmail.com>
2025-02-27 18:35:51 -0700

    Quick Sort Method
    Created The Quick Sort Method + The swap utility

--- assignment1StartingCode/src/sorting/SortingAlgorithms.java
+++ assignment1StartingCode/src/sorting/SortingAlgorithms.java
@@ -1,10 +1,8 @@
 package sorting;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class SortingAlgorithms {
-	
 	
     // Insertion Sort
     public static <T> void insertionSort(T[] array, Comparator<T> comparator) {
@@ -19,4 +17,35 @@ public class SortingAlgorithms {
             array[j + 1] = key;
         }
     }
+    
+    // Quick Sort
+    public static <T> void quickSort(T[] array, int low, int high, Comparator<T> comparator) {
+        if (low < high) {
+            int pi = partition(array, low, high, comparator);
+            quickSort(array, low, pi - 1, comparator);
+            quickSort(array, pi + 1, high, comparator);
+        }
+    }
+
+    private static <T> int partition(T[] array, int low, int high, Comparator<T> comparator) {
+        T pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (comparator.compare(array[j], pivot) >= 0) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+ 
+
+// Swap utility
+private static <T> void swap(T[] array, int i, int j) {
+    T temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
+}
+
